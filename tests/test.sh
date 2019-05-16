bae35545575b97edfe454cc0587ef1ce607d63e2
Caian R. Ertl <hi@caian.org>
2019-05-16 01:02:59 -0300

    test: include tests

---
+++ tests/test.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+set -e
+
+run_playbook() {
+    ansible-playbook -i hosts stow.yml --extra-vars "state=$1" -vvv
+}
+
+test_target_file() {
+    # test if the target file is a symbolic link
+    test -h "$HOME/.config/foo/bar" || exit 1
+
+    # test the file content
+    grep -Fxq bar "$HOME/.config/foo/bar" || exit 1
+}
+
+# create the library directory and copy the module
+mkdir library
+cp ../stow .
+
+# move the package to the home directory
+mv package ~
+
+# run the playbook and stow the package
+figlet "state: present"
+run_playbook "present"
+
+# test
+test_target_file
+
+# run the playbook and unstow the package
+figlet "state: absent"
+run_playbook "absent"
+
+# test if the target symlink were removed
+test -h "$HOME/.config/foo/bar" && exit 1
+
+# stow ,restow, unstow
+figlet "state: latest"
+run_playbook "present"
+run_playbook "latest"
+run_playbook "absent"
+
+# create a file that should conflict with the package
+mkdir -p "$HOME/.config/foo"
+echo "wrong" >> "$HOME/.config/foo/bar"
+
+# test if conflict
+figlet "conflict"
+run_playbook "present" && exit 1
+
+# stow (and override); test again
+run_playbook "supress"
+test_target_file
